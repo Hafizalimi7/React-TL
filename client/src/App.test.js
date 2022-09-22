130c0be77d8cd7537a3f27db295a996031e9a988
Hafiz <hafizalimi77@gmail.com>
2022-09-22 11:38:44 +0100

    did styling

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,16 @@
+import React from "react"
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/To Do List/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test ('renders input', () => {
+  const { getByTestId } = render(<App /> );
+  const inputEl = screen.getByTestId('inputId')
+
+  expect(inputEl.textContent).toBe("")
+})
