3621ac015202819d7c70918b4ff9976e4ab13382
Hafiz <hafizalimi77@gmail.com>
2022-09-22 12:48:32 +0100

    tested to see if you can add things to the db from backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require ('express')
 const app = express()
+const mysql = require("mysql")
+
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "toDoList"
+})
+
 
 app.get("/", (req,res) => {
-  res.send("Backend")
+  const sql = "INSERT INTO tasks (allTasks) VALUES ('COOKING');"
+  db.query(sql, (err,result) => {
+    console.log(`number of items entered database: ${result.affectedRows}`)
+    res.send("Backend")
+  })
 })
 
 app.listen(3001, () => {
