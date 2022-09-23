37268e5295dc591281d0dec3658e849f4615abd0
Hafiz <hafizalimi77@gmail.com>
2022-09-23 11:33:00 +0100

    add schema, connnected frontend to database *added schema *fixed cors error *connected front end to my sql, now can see input in db

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
+import React, {useState, useEffect} from "react"
+import Axios from "axios"
+
 
 function App() {
+  
+  const [todos,setTodos] = useState('')
+
+  function submitTodo(){
+    Axios.post('http://localhost:3001/api/insert', {
+      todo: todos
+    })
+  }
+
+
   return (
     <div className="App">
       <div className="form">
       <h1>To Do List</h1>
-        <input className="form input" type="text" data-testid="inputId"/>
-        <button>Submit</button>
+        <input 
+        className="form input" 
+        type="text" 
+        data-testid="inputId"
+        onChange={(e) => {
+          setTodos(e.target.value)
+        }}
+        />
+        <button onClick={submitTodo}>Submit</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require ('express')
 const app = express()
 const mysql = require("mysql")
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 
 const db = mysql.createPool({
@@ -10,14 +12,20 @@ const db = mysql.createPool({
   database: "toDoList"
 })
 
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get("/", (req,res) => {
-  const sql = "INSERT INTO tasks (allTasks) VALUES ('COOKING');"
-  db.query(sql, (err,result) => {
-    console.log(`number of items entered database: ${result.affectedRows}`)
-    res.send("Backend")
+app.post("/api/insert", (req,res) => {
+  const todo = req.body.todo
+
+  const sqlInsert = 
+    "INSERT INTO tasks (allTasks) VALUES (?);"
+  db.query(sqlInsert, todo, (err,result) => {
+    console.log(`number of item entered the database ${result.affectedRows}`)
   })
 })
+
 
 app.listen(3001, () => {
   console.log('running on port 3001')
