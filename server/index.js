c25279e39f0611fa4512b97c31b1780daaac823a
Hafiz Alimi <88586488+Hafizalimi7@users.noreply.github.com>
2022-10-03 18:11:33 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require ('express')
 const app = express()
-const mysql = require("mysql")
+const mysql = require("mysql") 
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
